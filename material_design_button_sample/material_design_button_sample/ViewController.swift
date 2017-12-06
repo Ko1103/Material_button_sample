bd085d13c4eb3d6151ddfb7c7df1a790af5fc55f
KO-YAMAKO <ko.foot1103@gmail.com>
2017-12-06 13:58:41 +0900

    add samples

---
+++ material_design_button_sample/material_design_button_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  material_design_button_sample
+//
+//  Created by 山浦功 on 2017/12/06.
+//  Copyright © 2017年 Blue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
